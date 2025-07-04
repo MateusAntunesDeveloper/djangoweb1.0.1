b1a73d857bca0cdc8642e10320fbc431a73cfb35
Mateus Antunes <144839912+MateusAntunesDeveloper@users.noreply.github.com>
2025-07-04 17:31:01 -0300

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,6 @@
+import CssSyntaxError_ from "postcss/lib/css-syntax-error";
+
+
 console.log("js its runn");
 alert("hello in js web browser");
 console.warn('This is warning');
@@ -5,7 +8,6 @@ function changeText(){
     const parameter = document.getElementById("message");
     parameter.innerText = "voce clicou aqui nesse botao";
 }
-
 function gmailConfirm(){
     const paragraph = document.getElementById("myParagraphTest");
     if (paragraph){
@@ -13,7 +15,22 @@ function gmailConfirm(){
         paragraph.style = "black";
         paragraph.style = "bold";
     }
-
-
-
+}
+function error_css(){
+    if(ValidityState == True){
+        alert("css validate");
+    }else{
+        return CssSyntaxError_
+    }
+}
+function passwordConfirm()
+{
+    const hash = document.getElementById("passwordTest");
+    if (hash){
+        hash.textContent = "password in test js";
+        //hash.accessKey(textContent);
+        hash.style = "black";
+        hash.style = "bold";
+    }
+    
 }
